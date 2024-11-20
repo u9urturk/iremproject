9f0e557bbe41e52e6c044e3aaeb789c07dc08ef4
u9urturk <u9urturk@gmail.com>
2024-11-20 10:53:41 +0300

    bug fix

--- src/pages/profile/IndexProfile.jsx
+++ src/pages/profile/IndexProfile.jsx
@@ -14,32 +14,32 @@ export default function IndexProfile() {
       phoneNumber: "",
       district: "",
       county: "",
-      emailVerified:false,
-      phoneVerified:false,
-      photoURL:""
+      emailVerified: false,
+      phoneVerified: false,
+      photoURL: ""
     }
   )
-  
+
   useEffect(() => {
-    if(user){
-      getUserByUid(user.uid).then(res=>{
+    if (user) {
+      getUserByUid(user.uid).then(res => {
         setForm({
-          displayName:res.displayName,
-          email:res.email,
-          phoneNumber:res.phoneNumber?res.phoneNumber:"",
-          county:res.county?res.county:"",
-          district:res.district?res.district:"",
-          emailVerified:res.emailVerified?res.emailVerified:false,
-          phoneVerified:res.phoneVerified?res.phoneVerified:false,
-          photoURL:res.photoURL?res.photoURL:"https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+          displayName: res.displayName,
+          email: res.email,
+          phoneNumber: res.phoneNumber ? res.phoneNumber : "",
+          county: res.county ? res.county : "",
+          district: res.district ? res.district : "",
+          emailVerified: res.emailVerified ? res.emailVerified : false,
+          phoneVerified: res.phoneVerified ? res.phoneVerified : false,
+          photoURL: res.photoURL ? res.photoURL : "https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
         });
       })
     }
   }, [user])
-  
 
-  const handleUpdateProfile=async ()=>{
-    await updateUserProfile(user.uid,form);
+
+  const handleUpdateProfile = async () => {
+    await updateUserProfile(user.uid, form);
   }
 
 
@@ -51,22 +51,20 @@ export default function IndexProfile() {
     }));
   };
   return (
-    <div className='w-[80%] animate-fade-left  py-4 space-y-16 flex rounded-md items-center justify-center flex-col h-auto'>
-      <div className=' py-2 px-3  w-full tracking-wider flex items-center justify-between rounded font-bold  bg-base-100'>
+    <div className='w-full animate-fade-left  px-8 py-4  space-y-8 flex rounded-md items-center justify-center flex-col h-auto'>
+      <div className=' py-3 px-4  w-full tracking-wider flex items-center justify-between rounded font-bold  bg-base-100'>
         <span>Profil</span>
         <div className='flex items-center justify-center gap-x-2'>
           <div clas><FaRegBell size={22} /></div>
-          <div className="avatar">
-            <div className="w-10 rounded">
-              <img src={form.photoURL} />
-            </div>
-          </div>
+
         </div>
       </div>
       <div className='flex items-center w-[90%] justify-center flex-col'>
-        <div className="avatar">
+        <div className="avatar pb-4">
           <div className="w-32 rounded">
-            <img src={form.photoURL}/>
+            {form.photoURL?<img className='animate-fade' src={form.photoURL} />
+              :<div className='w-full h-full bg-base-100 flex items-center justify-center'><span className="loading bg-primary loading-ring loading-lg"></span></div>
+          }
           </div>
         </div>
         <form className='w-full space-y-4' action="">
@@ -86,8 +84,8 @@ export default function IndexProfile() {
                 value={form.email}
                 onChange={handleChange}
                 placeholder="Email" className='w-full rounded input' />
-              <span className={`cursor-pointer tooltip ${form.emailVerified?"tooltip-success":"tooltip-warning"}`} data-tip={`${form.emailVerified?"E-Posta Doğrulandı.":"E-Posta Doğrulanmadı."}`}>
-                <FaCheckCircle className={`${form.emailVerified?"text-success":"text-warning"}`} size={24} />
+              <span className={`cursor-pointer tooltip ${form.emailVerified ? "tooltip-success" : "tooltip-warning"}`} data-tip={`${form.emailVerified ? "E-Posta Doğrulandı." : "E-Posta Doğrulanmadı."}`}>
+                <FaCheckCircle className={`${form.emailVerified ? "text-success" : "text-warning"}`} size={24} />
               </span>
             </div>
           </div>
@@ -99,8 +97,8 @@ export default function IndexProfile() {
                 value={form.phoneNumber}
                 onChange={handleChange}
                 placeholder="Telefon" className='w-full rounded input' />
-              <span className={`cursor-pointer tooltip ${form.emailVerified?"tooltip-success":"tooltip-warning"}`} data-tip={`${form.phoneVerified?"Numara Doğrulandı.":"Numara Doğrulanmadı."}`}>
-                <FaCheckCircle className={`${form.phoneVerified?"text-success":"text-warning"}`} size={24} />
+              <span className={`cursor-pointer tooltip ${form.emailVerified ? "tooltip-success" : "tooltip-warning"}`} data-tip={`${form.phoneVerified ? "Numara Doğrulandı." : "Numara Doğrulanmadı."}`}>
+                <FaCheckCircle className={`${form.phoneVerified ? "text-success" : "text-warning"}`} size={24} />
               </span>
             </div>
           </div>
--- src/pages/profile/MainProfile.jsx
+++ src/pages/profile/MainProfile.jsx
@@ -1,31 +1,35 @@
 import { ShoppingBasketIcon } from 'lucide-react'
 import React from 'react'
 import { FaRegAddressCard } from 'react-icons/fa'
+import { LuUserCog } from 'react-icons/lu'
 import { MdFavorite, MdNotifications } from 'react-icons/md'
 import { Link, Outlet } from 'react-router-dom'
 
 export default function MainProfile() {
   return (
     <>
-      <div className='w-full container flex items-start shadow-inner rounded-xl pl-3 md:pl-8 md:my-24 justify-center  h-auto'>
-        <div className='w-[10%] md:w-[20%] h-full sticky top-20 pt-8'>
+      <div className='w-full container flex items-start shadow-inner rounded-xl md:my-24 justify-start  h-auto'>
+        <div className='w-[10%] px-4 h-full sticky top-20 pt-8'>
           <ul className='flex flex-col gap-y-4  font-semibold opacity-60'>
-            <li className='cursor-pointer hover:scale-110 transition-transform' >
+            <Link className='cursor-pointer flex flex-col items-center justify-center hover:scale-110 transition-transform' to={"/profile"}>
+              <LuUserCog  size={24} /><p className='hidden md:block'>Profil</p>
+            </Link>
+            <li className='cursor-pointer flex flex-col items-center justify-center hover:scale-110 transition-transform' >
               <MdFavorite size={24}></MdFavorite><p className='hidden md:block'>Favoriler</p>
             </li>
-            <Link to={"orders"} className='cursor-pointer hover:scale-110 transition-transform'>
+            <Link to={"orders"} className='cursor-pointer flex flex-col items-center justify-center hover:scale-110 transition-transform'>
               <ShoppingBasketIcon size={24}></ShoppingBasketIcon><p className='hidden md:block'>Siparişler</p>
             </Link>
-            <li className='cursor-pointer hidden hover:scale-110 transition-transform'>İadeler</li>
-            <Link to={"address"} className='cursor-pointer hover:scale-110 transition-transform' >
+            <li className='cursor-pointer  flex-col items-center  justify-center hidden hover:scale-110 transition-transform'>İadeler</li>
+            <Link to={"address"} className='cursor-pointer flex flex-col items-center  hover:scale-110 transition-transform' >
               <FaRegAddressCard size={24}></FaRegAddressCard><p className='hidden md:block'>Adreslerim</p>
             </Link>
-            <li className='cursor-pointer hover:scale-110 transition-transform'>
+            <li className='cursor-pointer flex flex-col items-center justify-center hover:scale-110 transition-transform'>
               <MdNotifications size={24}></MdNotifications><p className='hidden md:block'>Bildirimlerim</p>
             </li>
           </ul>
         </div>
-        <div className='w-[90%] md:w-[80%] flex items-center pb-8 rounded-r-xl justify-center h-auto bg-base-300'>
+        <div className='w-[90%] flex items-center pb-8 rounded-r-xl justify-center h-auto bg-base-300'>
           <Outlet></Outlet>
         </div>
       </div>
