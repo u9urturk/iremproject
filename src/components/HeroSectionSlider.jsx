f7617d9b498f49882f73adc4ca917c4cf8699024
u9urturk <u9urturk@gmail.com>
2024-11-21 14:48:30 +0300

    Refactoring

--- src/components/HeroSectionSlider.jsx
+++ src/components/HeroSectionSlider.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Slider from "react-slick";
 
-export default function HeroSectionSlider({width="full",hight="72"}) {
+export default function HeroSectionSlider({width="full",hight="64"}) {
 
     const settings = {
         arrow: false,
